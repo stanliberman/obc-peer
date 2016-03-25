6fd58b5accc4c00a2d689c191e05412a0507ac95
cp68 <cp68@ntrs.com>
2016-03-25 13:48:23 +0000

    Use security name as shortname for contract, assume "init" is the init method, other clean up

--- openchain/example/chaincode/asset_manager/deploySecurityContract.sh
+++ openchain/example/chaincode/asset_manager/deploySecurityContract.sh
@@ -12,21 +12,23 @@ echo "--> Deploy asset contract... "
 #input arguments
 #	username
 #	directory containing chaincode source
-#	chain code ID 
-#	constructor name
 #	security identifier	
+#	dev mode (1=YES, 0=NO)
 #EXAMPLE:
-#	./deploySecurityContract.sh chrisP myChainCodeID github.com/openblockchain/obc-peer/openchain/example/chaincode/simpleFinancialSecurity/commonstock.go init "IBM"
+#	./deploySecurityContract.sh chrisP myChainCodeID github.com/openblockchain/obc-peer/openchain/example/chaincode/simpleFinancialSecurity/ "IBM" 1
 
 
 HOST=0.0.0.0
 PORT=30303
 
+#Incoming args
 chainCodeUser=$1
-chainCodeID=$2
-chainCodePath=$3
-constructorName=$4
-securityName=$5
+chainCodePath=$2
+securityName=$3
+devMode=$4
+
+deployMode="-p"
+deployName=$chainCodePath
 pid=-1
 
 registryName=`cat registry.address`
@@ -36,32 +38,33 @@ echo "Logging in $chainCodeUser..."
 cd $GOPATH/src/github.com/hyperledger-incubator/obc-peer
 ./obc-peer login $chainCodeUser 
 
-#Register the chaincode and put the process in the background
-#This is only really needed if running the OBC peer in dev mode
-fileName="$(basename $chainCodePath)" 
-directoryName=$GOPATH/src/$(dirname $chainCodePath)
-compiled=$(echo $fileName | cut -f 1 -d '.')
-echo "Move to $directoryName..."
-cd $directoryName
-echo "Building GO file $fileName..."
-go build
-OPENCHAIN_CHAINCODE_ID_NAME=$chainCodeID OPENCHAIN_PEER_ADDRESS=$HOST:$PORT $directoryName/$compiled &
-pid=$!
+directoryName=$GOPATH/src/$chainCodePath
+if [[ $devMode -eq 1 ]]; then
+	#Register the chaincode and put the process in the background
+	#This is only really needed if running the OBC peer in dev mode
+	fileExtension=".go"
+	fileName=`ls $directoryName/*.go`	
+	fileName=${fileName%$fileExtension}
+	echo "Move to $directoryName..."
+	cd $directoryName
+	echo "Building GO file $fileName..."
+	go build
+	OPENCHAIN_CHAINCODE_ID_NAME=$securityName OPENCHAIN_PEER_ADDRESS=$HOST:$PORT $fileName &
+	pid=$!
+	deployMode="-n"
+	deployName=$securityName
+fi
 
 #Deploy the chaincode
 cd $GOPATH/src/github.com/hyperledger-incubator/obc-peer
-echo "Deploying contract $chainCodeID with constructor $constructorName..."
-constructorJSON+='{"Function":"'
-constructorJSON+=$constructorName
-constructorJSON+='", "Args": ["'
+echo "Deploying contract $securityName with constructor init..."
+constructorJSON+='{"Function":"init", "Args": ["'
 constructorJSON+=$securityName
 constructorJSON+='"]}'
 echo "Constructor args: $constructorJSON"
-prefix=$GOPATH/src
-deployPath=${directoryName#*"$prefix"}
-./obc-peer chaincode deploy -u $chainCodeUser -p $deployPath --ctor="$constructorJSON" > $directoryName/$chainCodeID
-chainCodeAddress=$(cat $directoryName/$chainCodeID)
-rm $directoryName/$chainCodeID
+./obc-peer chaincode deploy -u $chainCodeUser $deployMode $deployName --ctor="$constructorJSON" > $directoryName/$securityName
+chainCodeAddress=$(cat $directoryName/$securityName)
+rm $directoryName/$securityName
 
 #Capture the contract hash and register it in the asset registry
 echo "Contract name: $chainCodeAddress"
@@ -69,10 +72,10 @@ invokeConstructor='{"Function": "register", "Args": ["'$securityName'", "'$chain
 ./obc-peer chaincode invoke -u $chainCodeUser -n $registryName -c "$invokeConstructor"
 
 #kill the local chain code that we started earlier if it is still running
-if [[ $pid > 0 ]] &&  ps -p $pid > /dev/null;
+if [[ $pid -eq 0 ]] &&  ps -p $pid > /dev/null ;
 then
 	kill $pid
 fi
 
-echo "Chaincode $chainCodeID has been compiled, registered and deployed!"
+echo "Chaincode $securityName has been compiled, registered and deployed!"
 
